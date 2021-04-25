88e077ebc716cf0e2cecbb61112a6c226e9134d0
ajay374-J <ajayjogdand374@gmail.com>
2021-04-25 09:52:57 +0530

    changes

--- next_quality/next_quality/custom_quality_inspection.py
+++ next_quality/next_quality/custom_quality_inspection.py
@@ -10,6 +10,21 @@ from erpnext.stock.doctype.quality_inspection.quality_inspection import QualityI
 import json
 
 class CustomQualityInspection(QualityInspection):
+    @frappe.whitelist()
+    def get_item_specification_details(self):
+        if not self.quality_inspection_template:
+            self.quality_inspection_template = frappe.db.get_value('Item',
+                self.item_code, 'quality_inspection_template')
+
+        if not self.quality_inspection_template: return
+
+        self.set('readings', [])
+        parameters = get_template_details(self.quality_inspection_template)
+        for d in parameters:
+            child = self.append('readings', {})
+            child.update(d)
+            child.status = "Accepted"
+
     def update_qc_reference(self):
         quality_inspection = self.name if self.docstatus == 1 else ""
 
