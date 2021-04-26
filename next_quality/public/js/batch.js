6e3e96748d6aa16619988cd0e4b40b1932d11786
ajay374-J <ajayjogdand374@gmail.com>
2021-04-26 22:04:11 +0530

    changes

--- next_quality/public/js/batch.js
+++ next_quality/public/js/batch.js
@@ -3,10 +3,70 @@ refresh:function(frm){
     if(frm.doc.disabled==1){
         frm.add_custom_button(__('Enable Batch'), function() {
                 
-                
-                
-        });
+            let d = new frappe.ui.Dialog({
+                fields: [
+                    {
+                        label: 'Add Comment',
+                        fieldname: 'add_comment',
+                        fieldtype: 'Small Text',
+                        reqd: 1
+                    }
 
+                ],
+                primary_action_label: 'Submit',
+                primary_action(values) {
+                    frappe.call({
+                        "method": "frappe.desk.form.utils.add_comment",
+                        "args": {
+                          reference_doctype: frm.doctype,
+                          reference_name: frm.docname,
+                          content:values.add_comment,
+                          comment_email: frappe.session.user,
+                          comment_by: frappe.session.user_fullname,
+                        }
+                    });
+                    d.hide();
+                    frm.set_value("disabled", 1);
+				    frm.refresh_field("disabled");
+
+                } 
+        });
+        d.show();
+    });
+
+    }
+    if(frm.doc.disabled==0){
+        frm.add_custom_button(__('Disable Batch'), function() {
+            let d = new frappe.ui.Dialog({
+                fields: [
+                    {
+                        label: 'Add Comment',
+                        fieldname: 'add_comment',
+                        fieldtype: 'Small Text',
+                        reqd: 1
+                    }
+
+                ],
+                primary_action_label: 'Submit',
+                primary_action(values) {
+                    frappe.call({
+                        "method": "frappe.desk.form.utils.add_comment",
+                        "args": {
+                          reference_doctype: frm.doctype,
+                          reference_name: frm.docname,
+                          content:values.add_comment,
+                          comment_email: frappe.session.user,
+                          comment_by: frappe.session.user_fullname,
+                        }
+                    });
+                    d.hide();
+                    frm.set_value("disabled", 1);
+				    frm.refresh_field("disabled");
+
+                } 
+        });
+        d.show();
+    });
     }
 }
 });
