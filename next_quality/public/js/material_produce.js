a0036d280191174f933b7459babd8b62a89eb2bf
ajay374-J <ajayjogdand374@gmail.com>
2021-04-30 16:55:40 +0530

    changes

--- next_quality/public/js/material_produce.js
+++ next_quality/public/js/material_produce.js
@@ -5,11 +5,11 @@ frappe.ui.form.on("Material Produce",{
   		if(d.copy_quality_inspection==0){
 			if(frm.doc.docstatus==0){
         		frm.add_custom_button(__('Quality Inspection'), function() {
-					if(frm.doc.quality_inspection_created==1)
-					{	
-						frappe.msgprint(__("Quality Inspection already Created"));
-					}
-					else{
+					// if(frm.doc.quality_inspection_created==1)
+					// {	
+					// 	frappe.msgprint(__("Quality Inspection already Created"));
+					// }
+					// else{
             		frm.call({
 					method:"next_quality.next_quality.custom_material_produce.create_inps",
 					args: {
@@ -23,7 +23,7 @@ frappe.ui.form.on("Material Produce",{
 						}
 					}
             	});
-			}
+			// }
 				frm.set_value("quality_inspection_created", 1);
 				frm.refresh_field("quality_inspection_created");
 				frm.save();
@@ -33,11 +33,11 @@ frappe.ui.form.on("Material Produce",{
 		else if(d.copy_quality_inspection==1){
 			if(frm.doc.docstatus==0){
 				frm.add_custom_button(__('Copy Quality Inspection'), function() {
-					if(frm.doc.quality_inspection_created==1)
-					{	
-						frappe.msgprint(__("Quality Inspection already Created"));
-					}
-					else{
+					// if(frm.doc.quality_inspection_created==1)
+					// {	
+					// 	frappe.msgprint(__("Quality Inspection already Created"));
+					// }
+					// else{
 					frm.call({
 					method:"next_quality.next_quality.custom_material_produce.copy_inps",
 					args: {
@@ -48,11 +48,12 @@ frappe.ui.form.on("Material Produce",{
 					callback: function(r) {
 						if (r.message) {
 							frappe.msgprint(__("Quality Inspection Created"));
-							frm.set_value("quality_inspection", r.message)
+							// frm.set_value("quality_inspection", r.message)
+							// frm.save()
 						}
 					}
 				});
-				}
+				// }
 				frm.set_value("quality_inspection_created", 1);
 				frm.refresh_field("quality_inspection_created");
 				frm.save();
