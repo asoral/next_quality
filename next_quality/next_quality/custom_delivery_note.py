022fff3af9862289a40a0a4e5c4c1acc321147c5
ajay374-J <ajayjogdand374@gmail.com>
2021-06-09 13:26:34 +0530

    changes

--- next_quality/next_quality/custom_delivery_note.py
+++ next_quality/next_quality/custom_delivery_note.py
@@ -26,7 +26,7 @@ def get_list(self,method):
                                         if float(row2.reading_1) <= row1.min_value or float(row2.reading_1) >= row1.max_value:
                                             frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
 
-                                elif row1.specification == row2.specification:
+                                if row1.specification == row2.specification:
                                     if not row2.reading_value and row1.numeric==0 and row1.formula_based_criteria==0 and row1.selection==0:
                                         frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement$")
                                     elif row2.reading_value and row1.value!=row2.reading_value:
@@ -35,18 +35,18 @@ def get_list(self,method):
                                     else:
                                         pass
                                 
-                                # elif row1.specification == row2.specification :
-                                #     if not row2.parameter_value and row1.selection==1:
-                                #         frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
-                                #     elif row2.parameter_value and row1.selection==1 and row2.selection==1:
-                                #         print(row1.get('values'))
-                                #         con_to_json = json.loads(row1.get('values'))
-                                #         b=[]
-                                #         for a in con_to_json:
-                                #             b.append(a.get('value'))
-                                #         if row2.parameter_value not in b:
-                                #             frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
-                                elif row1.specification == row2.specification :
+                                if row1.specification == row2.specification :
+                                    if not row2.parameter_value and row1.selection==1:
+                                        frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
+                                    elif row2.parameter_value and row1.selection==1 and row2.selection==1:
+                                        print(row1.get('values'))
+                                        con_to_json = json.loads(row1.get('values'))
+                                        b=[]
+                                        for a in con_to_json:
+                                            b.append(a.get('value'))
+                                        if row2.parameter_value not in b:
+                                            frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
+                                if row1.specification == row2.specification :
                                     if not row2.reading_1 and row1.numeric==1 and row1.formula_based_criteria==1 :
                                         frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
                                     elif row2.reading_1 and row1.numeric==1 and row2.numeric==1 and row1.formula_based_criteria==1 and row2.formula_based_criteria==1:
@@ -70,7 +70,7 @@ def get_list(self,method):
                                             pass
                                         else:
                                             frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
-                                elif row1.specification == row2.specification:
+                                if row1.specification == row2.specification:
                                     if not row2.reading_value and row1.numeric==0 and row1.formula_based_criteria==1:
                                         frappe.throw("Please Select another Batch No quality inspection didn't match As per customer requirement")
                                     elif row2.reading_value and row1.numeric==0 and row2.numeric==0 and row1.formula_based_criteria==1 and row2.formula_based_criteria==1:
