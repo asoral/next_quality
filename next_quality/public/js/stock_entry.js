a106b9f3527455bbb4f08d054e7a0669be96f015
ajay374-J <ajayjogdand374@gmail.com>
2021-06-15 17:26:56 +0530

    changes

--- next_quality/public/js/stock_entry.js
+++ next_quality/public/js/stock_entry.js
@@ -1,25 +1,25 @@
-// frappe.ui.form.on("Stock Entry", {
-//     onload: function(frm){
-//         if(frm.doc.docstatus == 0 && frm.doc.inspection_required)
-//         {
-//             frm.add_custom_button(__("Make Quality Inspection"), function() {
-//                 frappe.call({
-//                     method: 'next_quality.next_quality.custom_quality_inspection.make_stock_quality_inspec',
-//                     args: {
-//                         "doc_name" : frm.doc.name,
-//                         "doctype" : frm.doctype
-//                     },
-//                     callback: function(r) {
-//                         if (r.message)
-//                         {
-//                             frappe.msgprint(__("Quality Inspection Created"));
-//                         }
-//                     }
+frappe.ui.form.on("Stock Entry", {
+    onload: function(frm){
+        if(frm.doc.docstatus == 0 && frm.doc.inspection_required)
+        {
+            frm.add_custom_button(__("Make Quality Inspection"), function() {
+                frappe.call({
+                    method: 'next_quality.next_quality.custom_quality_inspection.make_stock_quality_inspec',
+                    args: {
+                        "doc_name" : frm.doc.name,
+                        "doctype" : frm.doctype
+                    },
+                    callback: function(r) {
+                        if (r.message)
+                        {
+                            frappe.msgprint(__("Quality Inspection Created"));
+                        }
+                    }
                     
-//                 });
+                });
             
   
-//             },'Create');
-//         }
-//     }
-// });
+            },'Create');
+        }
+    }
+});
