fd2450cf8e44b2953085d41c4361b3f63c34221f
Satish <sshingane@dexciss.com>
2025-02-11 16:43:12 +0530

    add filters sales order

--- next_quality/public/js/sales_order.js
+++ next_quality/public/js/sales_order.js
@@ -1,5 +1,36 @@
 frappe.ui.form.on("Sales Order", {
     refresh: function(frm){
+        console.log("Trigger Sales Order------");
+
+        if (frm.doc.company) {
+            frm.set_query('department', function() {
+                return {
+                    filters: {
+                        company: frm.doc.company
+                    }
+                };
+            });
+        }
+
+        if (frm.doc.company === "Subhash Chemical Industries Pvt Ltd") {
+            frm.set_query("set_warehouse", function() {
+                return {
+                    filters: {
+                        name: ["in",["FG UNIT 1 - SCIPL", "FG UNIT 2 - SCIPL", "PACKING UNIT 1 - SCIPL", "PACKING UNIT 2 - SCIPL"] ]
+                    }
+                };
+            });
+        }
+
+        if (frm.doc.company === "JORINCO SPECIALITIES PVT LTD.") {
+            frm.set_query("set_warehouse", function() {
+                return {
+                    filters: {
+                        name: ["in", ["FG - JSPL", "PACKING - JSPL"]]
+                    }
+                };
+            });
+        }
         if(frm.doc.docstatus == 1){
             frm.add_custom_button(__("Customer Quality Inspection"), function() {
                 frappe.call({
