a9fba6a99b1979efb3b1eac3f1f9483af329214f
Satish Shingane <sshingane@dexciss.com>
2026-08-17 14:56:12 +0530

    fix: add pyproject.toml and remove frappe from requirements.txt for Frappe v15

--- next_quality/__init__.py
+++ next_quality/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
-__version__ = '0.0.1'
-from erpnext.stock import get_item_details
-from erpnext.stock.doctype.quality_inspection.quality_inspection import QualityInspection
-from next_quality.custom_get_item_details import custom_validate_item_details,custom_on_trash
 
-get_item_details.validate_item_details = custom_validate_item_details
-QualityInspection.on_trash = custom_on_trash
+__version__ = '0.0.1'
+
+try:
+    from erpnext.stock import get_item_details
+    from erpnext.stock.doctype.quality_inspection.quality_inspection import QualityInspection
+    from next_quality.custom_get_item_details import custom_validate_item_details, custom_on_trash
+
+    get_item_details.validate_item_details = custom_validate_item_details
+    QualityInspection.on_trash = custom_on_trash
+except ImportError:
+    pass
